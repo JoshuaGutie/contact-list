a0b99ef8d41f3f1e1830c4e3008c5d0ed3eaf574
Josh Gutierrez <joshgutierrez@Joshs-MacBook-Air.local>
2020-05-31 08:00:07 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -2,9 +2,51 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function People (props) {
-  console.log("person", props)
-  return(<div>{props.People}</div>)
+// function People (props) {
+//   console.log("person", props)
+//   return(<div>{props.People}</div>)
+// }
+
+class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSelected: false
+    };
+  }
+
+  detailsClick = () => {
+    this.setState({
+      isSelected: !this.state.isSelected
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div style={{display: "flex", flexGrow: "4", flexDirection: "row", justifyContent: "center", background:"grey", margin:"10px"}}>
+        <h2>{this.props.person.name.first}</h2>
+        <img style={{margin: '5px', width: "200px"}} src={this.props.person.picture.thumbnail} />
+        <b><button style={{background: "orange", color: "blue", fontSize: "20px"}} onClick={this.detailsClick}>{this.state.isSelected ? 'show info' : ' hide info'}</button> </b>
+        <div class="details">
+          {
+            this.state.isSelected
+              ?
+              <div>
+                <div ><p>Gender: </p>{this.props.person.gender}</div>
+                <div ><p>Email: </p>{this.props.person.email}</div>
+                <div ><p>Phone: </p>{this.props.person.phone}</div>
+              </div>
+              :''
+          }
+        </div>
+      </div>
+
+    )
+  }
 }
 
 
@@ -12,29 +54,29 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-    people: [{}]
+    people: []
     }
   }
-componentDidMount(){
-fetch('https://randomuser.me/api?results=25')
-.then(json => json.json())
-.then(data =>{
-  this.setState({ 
-    people: data
-  }); console.log(data)
-  
-})
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=25')
+      .then(json => json.json())
+      .then(json => json.results)
+      .then(data => {
+        this.setState({
+          people: data
+        });
+        console.log(data);
+      })
 
 }
 
-  render (){
+render() {
   return (
-    <div className="App"> 
-      <People people = {this.state.people[0]}/>
-    <header className="App-header">
-     
-    </header>
-  </div>
+    <div className="App" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+      {this.state.people.map((userData, index) => (
+        <Contact key={index} person={userData} />
+      ))}
+    </div>
   );
 }
 }
